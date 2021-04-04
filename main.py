eb0d7b1b53b9cea65ad438457e25b2d35d8126d9
andr1976 <58475535+andr1976@users.noreply.github.com>
2021-04-04 23:10:16 +0200

    Added copyright

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# HydDown hydrogen/other gas depressurisation
+# Copyright (c) 2021 Anders Andreasen
+# Published under an MIT license
+
 from CoolProp.CoolProp import PropsSI
 import math
 import numpy as np
