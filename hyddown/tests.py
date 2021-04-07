2e2d4fdbef08cde4d3ac79305f01aaa63abaf30c
andr1976 <58475535+andr1976@users.noreply.github.com>
2021-04-08 00:09:18 +0200

    adding testing WIP

---
+++ hyddown/tests.py
@@ -0,0 +1,16 @@
+from hyddown import transport as tp
+
+def test_orifice():
+    assert 3==3
+
+def test_controlvalve():
+    assert 3==3
+
+def test_psv():
+    assert 3==3
+
+def test_h():
+    assert 3==3
+
+def test_NGr():
+    assert 3==3
