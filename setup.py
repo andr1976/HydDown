a5654c886b77ed55bb3ef320cbd5b0dcb7aaeb19
Anders Andreasen <58475535+andr1976@users.noreply.github.com>
2021-07-26 00:02:53 +0200

    Create setup.py
    Prepare for PyPi

---
+++ setup.py
@@ -0,0 +1,12 @@
+ from setuptools import setup
+
+ setup(
+   name='HydDown',
+   version='0.14',
+   author='Anders Andreasen',
+   author_email='andr1976@gmail.com',
+   packages=['hyddown'],
+   url='https://test.pypi.org/project/HydDown/',
+   license='LICENSE.txt',
+   description='Test',
+)
