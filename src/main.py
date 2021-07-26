b024edadf5744bb001be5a6db1aaf6eb12611edf
andr1976 <58475535+andr1976@users.noreply.github.com>
2021-07-26 23:53:44 +0200

    Small fix to pytest path

---
+++ src/main.py
@@ -0,0 +1,42 @@
+# HydDown hydrogen/other gas depressurisation
+# Copyright (c) 2021 Anders Andreasen
+# Published under an MIT license
+
+import yaml
+import sys
+from hyddown import HydDown
+import time
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        input_filename = sys.argv[1]
+    else:
+        input_filename = "input.yml"
+
+    with open(input_filename) as infile:
+        input = yaml.load(infile, Loader=yaml.FullLoader)
+
+
+    hdown=HydDown(input)
+    start = time.time()
+    hdown.run()
+    end = time.time()
+    print('Elapsed time: ',end-start,' sec.')
+    hdown.plot()
+
+    hdown.fluid.build_phase_envelope("None")
+    PE=hdown.fluid.get_phase_envelope_data()
+
+    import pylab as plt
+
+    plt.figure(2)
+    plt.plot(PE.T, PE.p, '-', label = 'HEOS Phase Envelope', color = 'g')
+    plt.plot(hdown.T_fluid,hdown.P,'-.',label = 'P/T fluid trajectory', color = 'b' )
+    plt.plot(hdown.T_fluid[0],hdown.P[0],'o',label = 'Start', color = 'b' )
+    plt.plot(hdown.T_fluid[-1],hdown.P[-1],'.',label = 'End', color = 'b' )
+    plt.xlabel('Temperature [K]')
+    plt.ylabel('Pressure [Pa]')
+    plt.legend(loc='best')
+    plt.tight_layout()
+    plt.show()
